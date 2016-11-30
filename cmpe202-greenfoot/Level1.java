2d233870ad3611ec7f19b45f1011d6cd37a7d683
Aaron Lam <dungll@uci.edu>
2016-11-29 16:53:04 -0800

    currently working version

---
+++ cmpe202-greenfoot/Level1.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Level1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Level1 extends Button
+{
+    String buttonText = "Level 1";
+    boolean clicked;
+
+    public Level1 ()
+    {
+        super("Level 1");
+    }
+    /**
+     * Act - do whatever the Level1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ cmpe202-greenfoot/Level2.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Level1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Level2 extends Button
+{
+    String buttonText = "Level 2";
+    boolean clicked;
+
+    public Level2 ()
+    {
+        super("Level 2");
+    }
+    /**
+     * Act - do whatever the Level1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
