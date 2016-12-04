cef4ea0041279eb5ea562acd3a8f32cf61937d46
aaronlam88 <aaron.y.lam@sjsu.edu>
2016-12-03 16:31:13 -0800

    minor update

--- cmpe202-greenfoot/GameWorld.java
+++ cmpe202-greenfoot/GameWorld.java
@@ -69,11 +69,6 @@ public class GameWorld extends World
         int i= 0, j=0;
         for (i = 40; i < size - 52; i += 52) {
             for (j = 80; j < size - 52; j += 52) {
-                // if (rand.nextBoolean()) {
-                    // addObject (new BlueCard (), i, j);
-                // } else {
-                    // addObject (new RedCard (), i, j);
-                // }
                 addObject (new Card(), i, j);
             }
             addObject (new BlankCard(), i, j);
@@ -83,10 +78,10 @@ public class GameWorld extends World
             addObject (new BlankCard(), i, j);
         }
         j += 10;
-        addObject(new StartButton(), (size-20)/2, j);
+        addObject(ButtonFactory.getButton("Start"), (size-20)/2, j);
         j += 50;
-        addObject(new SubmitButton(), (size-20)/2, j);
+        addObject(ButtonFactory.getButton("Submit"), (size-20)/2, j);
         j += 50;
-        addObject(new NextLevelButton(), (size-20)/2, j);
+        addObject(ButtonFactory.getButton("Next"), (size-20)/2, j);
     }
 }
--- cmpe202-greenfoot/InitState.java
+++ cmpe202-greenfoot/InitState.java
@@ -6,25 +6,5 @@
  */
 public class InitState extends State 
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class InitState
-     */
-    public InitState()
-    {
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
